a191f35904e0a4380e41fd161a8a220c4ac8c0d8
wander.porto <wanderwang@hotmail.com.br>
2020-06-04 22:48:51 -0300

    subindo projeto

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+import path from 'path';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333);
